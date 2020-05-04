7c7b0b10bd19079d04edcd7dc543c733b07b0e87
venkatbalaji <venkatb2206@gmail.com>
2020-05-04 18:11:13 +0530

    Add files via upload

--- newModuleJSON/index.js
+++ newModuleJSON/index.js
@@ -7,15 +7,15 @@ async function getDataFromAPI() {
   const response = await fetch(url);
   
   const data = await response.json();
-  console.log(data);
-  const a = [];
-  a.push(data[0]);
-  a.push(data[1]);
-  a.push(data[2]);
-  a.push(data[3]);
-  a.push(data[5]);
-console.log(a);
-  a.forEach(countryCapital => 
+  //console.log(data);
+//   const a = [];
+//   a.push(data[0]);
+//   a.push(data[1]);
+//   a.push(data[2]);
+//   a.push(data[3]);
+//   a.push(data[5]);
+// console.log(a);
+data.forEach(countryCapital => 
 {
         const Url_call = `https://api.openweathermap.org/data/2.5/weather?q=${countryCapital.capital}&appid=${API_KEY}`;
         fetch(Url_call)
